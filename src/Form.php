32dc45643b413ac9f3027cbcb862b87c1f9df1cc
irpsv <i.rpsv@live.com>
2016-08-22 16:15:17 +0500

    add Form

---
+++ src/Form.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Jugger\Form;
+
+/**
+ * Description of Form
+ *
+ * @author Ilya Rupasov <i.rpsv@live.com>
+ */
+class Form
+{
+    public $id;
+    
+    protected $attributes;
+    protected $handler;
+    protected $errors;
+
+    public function load(array $fields) {
+        
+    }
+    
+    public function validate() {
+        
+    }
+    
+    public function process() {
+        
+    }
+}
